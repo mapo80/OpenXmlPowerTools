40c7e4e4542bb9450146fa04fda68a1ac4cf6cde
Stefan Seeland <168659+stesee@users.noreply.github.com>
2025-03-08 22:57:08 +0100

    Added Win11 specific expected diff
    https://github.com/Codeuctivity/OpenXmlPowerTools/issues/134

---
+++ OpenXmlPowerTools.Tests/OpenXMLWordprocessingMLToHtmlConverter/AllowedDiffInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Codeuctivity.Tests.OpenXMLWordProcessingMLToHtmlConverter
+{
+    internal class AllowedDiffInfo
+    {
+        public bool DiffFileExists;
+        public string NewDiffImageFileName;
+        public string[] ExistingDiffImageFilename;
+
+        public AllowedDiffInfo(bool diffFileExists, string newDiffImageFileName, string[] matchingFiles)
+        {
+            DiffFileExists = diffFileExists;
+            NewDiffImageFileName = newDiffImageFileName;
+            ExistingDiffImageFilename = matchingFiles;
+        }
+    }
+}
